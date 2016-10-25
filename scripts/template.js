99d1cb48149aed92534cb9ce116637ea59cf862b
Jeff King <king.jeffey@gmail.com>
2016-10-24 18:10:38 -0700

    minor fixes around the files. added javascript constructor

---
+++ scripts/template.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var webPage = {};
+
+function WebPage(option) {
+  this.name = option.name;
+  this.url = option.url;
+  this.date = option.date;
+  this.discription = option.discription;
+}
